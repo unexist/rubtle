095303134be3059efb279230b01b6f59c38dc126
unexist <unexist@subforge.org>
2020-06-11 18:58:39 +0200

    Re-enabled tests and added implementation for bool and number

--- rubtle-lib/src/tests/rubtle.rs
+++ rubtle-lib/src/tests/rubtle.rs
@@ -12,7 +12,20 @@
 use crate::{Rubtle, Value};
 
 #[test]
-#[ignore]
+fn push_and_pop_bool_value() {
+    let rubtle = Rubtle::new();
+
+    let rval = Value::from(true);
+
+    rubtle.push_value(rval);
+    let rval2 = rubtle.pop_value();
+
+    println!("{:?}", rval);
+
+    assert_eq!(rval, rval2);
+}
+
+#[test]
 fn push_and_pop_number_value() {
     let rubtle = Rubtle::new();
 
