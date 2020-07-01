d0c4d4c462e81b71618ad3ab9cee4a51d0a67940
unexist <unexist@subforge.org>
2020-07-01 12:16:10 +0200

    Updated docs, tests and added Rubtle#push_global_value with test

--- rubtle-lib/src/tests/rubtle.rs
+++ rubtle-lib/src/tests/rubtle.rs
@@ -14,7 +14,6 @@ use crate::{Rubtle, Value};
 #[test]
 fn push_and_pop_bool_value() {
     let rubtle = Rubtle::new();
-
     let rval = Value::from(true);
 
     rubtle.push_value(&rval);
@@ -26,7 +25,6 @@ fn push_and_pop_bool_value() {
 #[test]
 fn push_and_pop_number_value() {
     let rubtle = Rubtle::new();
-
     let rval = Value::from(4);
 
     rubtle.push_value(&rval);
@@ -38,13 +36,36 @@ fn push_and_pop_number_value() {
 #[test]
 fn push_and_pop_string_value() {
     let rubtle = Rubtle::new();
-
-    let rval = Value::from("Test");
+    let rval = Value::from("rubtle");
 
     rubtle.push_value(&rval);
     let rval2 = rubtle.pop_value();
 
     assert_eq!(rval, rval2);
+}
+
+#[test]
+fn push_global_bool_value() {
+    let rubtle = Rubtle::new();
+    let rval = Value::from(true);
+
+    rubtle.push_global_value("rubtle", &rval);
+}
+
+#[test]
+fn push_global_number_value() {
+    let rubtle = Rubtle::new();
+    let rval = Value::from(4);
+
+    rubtle.push_global_value("rubtle", &rval);
+}
+
+#[test]
+fn push_global_string_value() {
+    let rubtle = Rubtle::new();
+    let rval = Value::from("rubtle");
+
+    rubtle.push_global_value("rubtle", &rval);
 }
 
 #[test]
