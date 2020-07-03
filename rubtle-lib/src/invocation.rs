8f65b8103adbd39dda70d28f0dc622cf79ef0071
unexist <unexist@subforge.org>
2020-07-03 17:33:57 +0200

    Added more types

---
+++ rubtle-lib/src/invocation.rs
@@ -0,0 +1,18 @@
+///
+/// @package Rubtle-Lib
+///
+/// @file Invocation functions
+/// @copyright 2020 Christoph Kappel <unexist@subforge.org>
+/// @version $Id$
+///
+/// This program can be distributed under the terms of the GNU GPLv2.
+/// See the file LICENSE for details.
+//
+
+use crate::{Rubtle, Value};
+
+pub struct Invocation<'rubtle> {
+    pub rubtle: &'rubtle Rubtle,
+    pub this: Value,
+    pub args: Vec<Value>,
+}
