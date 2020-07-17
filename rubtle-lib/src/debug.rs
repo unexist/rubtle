4c2b28c378d832a81c6992091b898d39da58e4a8
unexist <christoph@unexist.dev>
2020-07-17 14:26:20 +0200

    Re-organized files

---
+++ rubtle-lib/src/debug.rs
@@ -0,0 +1,33 @@
+///
+/// @package Rubtle-Lib
+///
+/// @file Debug functions
+/// @copyright 2020 Christoph Kappel <unexist@subforge.org>
+/// @version $Id$
+///
+/// This program can be distributed under the terms of the GNU GPLv2.
+/// See the file LICENSE for details.
+//
+use std::slice;
+
+use cesu8::from_cesu8;
+
+pub fn debug_stack(ctx: *mut ffi::duk_context, filename: &str, lineno: u32) {
+    unsafe {
+        ffi::duk_push_context_dump(ctx);
+
+        let mut len: ffi::duk_size_t = 0;
+        let data = ffi::duk_safe_to_lstring(ctx, -1, &mut len);
+
+        if !data.is_null() {
+            let bytes = slice::from_raw_parts(data as *const u8, len as usize);
+
+            match from_cesu8(bytes) {
+                Ok(string) => println!("\n{}@{}: {}", filename, lineno, string),
+                Err(_) => unreachable!(),
+            }
+        }
+
+        ffi::duk_pop(ctx);
+    }
+}
