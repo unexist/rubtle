3bdd929d6bff4d5452c7ccce2df003ed11bffde4
unexist <unexist@subforge.org>
2020-07-22 13:35:35 +0200

    Merged scripts

--- watch.sh
+++ watch.sh
@@ -1,1 +1,9 @@
-cargo watch -d 4 -x "test -p rubtle-lib -- --test-threads 1 --nocapture"
+#!/bin/zsh
+CARGOFLAGS="-c -d 4 -q --postpone"
+TESTFLAGS="--test-threads 1 --nocapture"
+
+if [ "$#" -ne 1 ] ; then
+    cargo watch $(echo $CARGOFLAGS) -x "test -p rubtle-lib -- $TESTFLAGS"
+else
+    RUSTFLAGS="-Z sanitizer=address" cargo watch $(echo $CARGOFLAGS) -x "test -p rubtle-lib -- $TESTFLAGS"
+fi
