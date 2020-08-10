538b8726689ab39584b95747845f5603a33031e6
unexist <christoph@unexist.dev>
2020-08-10 17:10:54 +0200

    Updated watch script

--- watch.sh
+++ watch.sh
@@ -1,9 +1,10 @@
 #!/bin/zsh
-CARGOFLAGS="-c -d 4 -q --postpone"
+CARGOFLAGS="-c -d 4 -q"
 TESTFLAGS="--test-threads 1 --nocapture"
 
 if [ "$#" -ne 1 ] ; then
     cargo watch $(echo $CARGOFLAGS) -x "test -p rubtle-lib -- $TESTFLAGS"
 else
     RUSTFLAGS="-Z sanitizer=address" cargo watch $(echo $CARGOFLAGS) -x "test -p rubtle-lib -- $TESTFLAGS"
+    #RUST_BACKTRACE=1 RUSTFLAGS="-Z sanitizer=address" cargo watch $(echo $CARGOFLAGS) -x "test -p rubtle-lib -- $TESTFLAGS"
 fi
