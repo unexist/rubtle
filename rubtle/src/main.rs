f3588a27eda0afb6880d2b72ea242bcff5cc508d
Christoph Kappel <christoph@unexist.dev>
2023-02-22 11:31:28 +0100

    Updated copyright year

--- rubtle/src/main.rs
+++ rubtle/src/main.rs
@@ -2,7 +2,7 @@
 /// @package Rubtle
 ///
 /// @file Rubtle main entry
-/// @copyright 2020-2021 Christoph Kappel <christoph@unexist.dev>
+/// @copyright 2020-present Christoph Kappel <christoph@unexist.dev>
 /// @version $Id$
 ///
 /// This program can be distributed under the terms of the GNU GPLv2.
